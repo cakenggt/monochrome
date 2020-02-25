033fc4fa88d21f5ddfa51316e05607cffcd1ae1a
Alec Lownes <alec.lownes@gmail.com>
2020-02-25 16:51:53 -0500

    Change to script-based approach

---
+++ script.js
@@ -0,0 +1,87 @@
+const fs = require("fs");
+const { writeFileSync } = fs;
+
+const black = "#0A0A0A";
+const blue = "#040080";
+const white = "#FFFFFF";
+
+const totalJSON = {
+  name: "Paper",
+  type: "light",
+  colors: {
+    ["editor.background"]: white,
+    ["editor.foreground"]: black,
+  },
+  tokenColors: [
+    {
+      name: "Regular",
+      scope: [
+        "keyword", // Keywords
+        "storage", // Keywords
+        "keyword.operator", // Operators
+        "variable", // Variables
+        "storage.type.function.arrow", // Arrow Function Arrows
+        "constant", // Numbers and Characters
+        "meta.template.expression", // Template Expressions
+        "punctuation", // Punctuation
+        "meta.tag", // HTML Tags
+        "punctuation.definition.tag.html", // HTML Tags
+        "punctuation.definition.tag.begin.html", // HTML Tags
+        "punctuation.definition.tag.end.html", // HTML Tags
+      ],
+      settings: { fontStyle: "" },
+    },
+    {
+      name: "Italic",
+      scope: [
+        "comment",
+        "punctuation.definition.comment",
+        "comment.block.preprocessor",
+        "comment.documentation",
+        "comment.block.documentation",
+        "string.quoted.docstring",
+        "keyword.control.conditional",
+        "support",
+        "storage.type",
+        "constant.language",
+        "variable.language",
+        "variable.parameter.function.language.special",
+        "string",
+        "meta.tag entity.other.attribute-name",
+        "entity.other.attribute - name.html",
+      ],
+      settings: { fontStyle: "italic" },
+    },
+    {
+      name: "Bold",
+      scope: [
+        "support.function",
+        "entity.name.function",
+        "entity.name.type",
+        "entity.other.inherited-class",
+        "punctuation.accessor",
+        "entity.name.tag",
+        "support.class.component.tsx",
+        "support.class.component.jsx",
+      ],
+      settings: { fontStyle: "bold" },
+    },
+    {
+      name: "Underline",
+      scope: ["keyword.control.flow"],
+      settings: { fontStyle: "underline" },
+    },
+  ],
+};
+
+writeFileSync("./themes/paper.json", JSON.stringify(totalJSON, undefined, 2));
+
+totalJSON.name = "Blueprint";
+totalJSON.type = "dark";
+totalJSON.colors["editor.background"] = blue;
+totalJSON.colors["editor.foreground"] = white;
+
+writeFileSync(
+  "./themes/blueprint.json",
+  JSON.stringify(totalJSON, undefined, 2),
+);
